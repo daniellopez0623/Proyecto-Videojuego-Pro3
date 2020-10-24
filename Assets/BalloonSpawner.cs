b43616326faa4f073d77671daf82d0e467440eb1
Josuel <josueltolentino@gmail.com>
2020-10-24 16:47:12 -0400

    Primer commit

---
+++ Assets/BalloonSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject balloonPrefab;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0.0f, .6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn(){
+        Instantiate(balloonPrefab, transform.position, transform.rotation);
+    }
+}
